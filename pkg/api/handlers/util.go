4d26d35a108a24a96e0d4da8f8939448ada3344d
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-06-22 08:43:00 -0300

    feat: handle util functions
    feat: handle util functions

---
+++ pkg/api/handlers/util.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/rafaelsanzio/go-consuming-soap/pkg/applog"
+	"github.com/rafaelsanzio/go-consuming-soap/pkg/errs"
+)
+
+var jsonMarshal = json.Marshal
+
+func fakeMarshal(v interface{}) ([]byte, error) {
+	return []byte{}, errs.ErrMarshalingJson
+}
+
+func restoreMarshal(replace func(v interface{}) ([]byte, error)) {
+	jsonMarshal = replace
+}
+
+var write = http.ResponseWriter.Write
+
+func fakeWrite(http.ResponseWriter, []byte) (int, error) {
+	return 0, errs.ErrResponseWriter
+}
+
+func restoreWrite(replace func(http.ResponseWriter, []byte) (int, error)) {
+	write = replace
+}
+
+var atoi = strconv.Atoi
+
+func fakeAtoi(s string) (int, error) {
+	return 0, errs.ErrConvertingStringToInt
+}
+
+func restoreAtoi(replace func(s string) (int, error)) {
+	atoi = replace
+}
+
+var httpGet = http.Get
+
+func fakeHttpGet(url string) (resp *http.Response, err error) {
+	return nil, errs.ErrConvertingStringToInt
+}
+
+func restoreHttpGet(replace func(url string) (resp *http.Response, err error)) {
+	httpGet = replace
+}
+
+func validateCountryCode(countryCode string) error {
+	if countryCode == "" || len(countryCode) != 2 {
+		return errs.ErrGettingParam.Throwf(applog.Log, errs.ErrFmt, "param with bad format")
+	}
+
+	res, err := httpGet(fmt.Sprintf("https://restcountries.com/v3.1/alpha/%s", countryCode))
+	if err != nil {
+		return errs.ErrNewRequest.Throwf(applog.Log, errs.ErrFmt, err)
+	}
+
+	if res.StatusCode == http.StatusNotFound {
+		return errs.ErrCountryNotFound.Throwf(applog.Log, errs.ErrFmt, fmt.Sprintf("country code: %s", countryCode))
+	}
+
+	return nil
+}
