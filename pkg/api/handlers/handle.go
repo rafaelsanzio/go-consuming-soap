c362084c73fa7901d91731bcafb8800e4e991e60
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-06-24 09:10:50 -0300

    feat: implement rate limiter
    feat: implement rate limiter

--- pkg/api/handlers/handle.go
+++ pkg/api/handlers/handle.go
@@ -2,14 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"time"
+
+	"golang.org/x/time/rate"
 
 	"github.com/rafaelsanzio/go-consuming-soap/pkg/applog"
+	"github.com/rafaelsanzio/go-consuming-soap/pkg/errs"
 )
+
+var rateLimit = rate.NewLimiter(rate.Every(10*time.Second), 3) // 10 request every 10 seconds
 
 func HandleAdapter(hf http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		applog.Log.Infof("Requesting - Method: %s, URL %s", r.Method, r.URL)
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+		if rateLimitAllow() == false {
+			applog.Log.Warnf("Rate limit to requests was exceed")
+			errs.HttpToManyRequests(w)
+			return
+		}
+
 		hf(w, r)
 	}
 }
